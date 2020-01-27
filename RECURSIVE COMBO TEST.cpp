5e0c0b350fe4ab7668bd987f847ed828ef9b1683
Aaron <avanclea@cs.uoregon.edu>
2020-01-26 23:40:25 -0800

    n choose m successfully implemented, but I'm wary about efficiency because I might have to use it a lot. Note to myself to see if I can improve it given that n choose m is kind of symmetric

--- RECURSIVE COMBO TEST.cpp
+++ RECURSIVE COMBO TEST.cpp
@@ -14,6 +14,39 @@ int fact(int n)
     return result;
 }
 
+//Can't have k greater than n
+//this combinatoric method will help me find an efficient data storage encoding in the pixels (I hope)
+int choose(int n, int k)
+{
+    //pigeonhole principle!
+    if(k > n)
+    {
+        return -1;
+    }
+
+    int m = n - k;
+
+    int j = fact(k);
+
+    int result = 1;
+
+    //modify the range to avoid calculating 3 factorials (does two instead)
+    for(int i = n; i > m; i--)
+    {
+        result *= i;
+    }
+
+    //this ought to always be an int but who knows
+    return (int)(result / j);
+
+
+
+
+}
+
+
+
+
 int main()
 {
     printf("Factorial tests\n");
@@ -21,6 +54,17 @@ int main()
     for(int i = 0; i < 11; i++)
     {
         printf("%2i : %i\n", i, fact(i));
+    }
+
+    printf("\n");
+
+    int n = 8;
+
+    printf("n choose m tests with n = %i\n", n);
+
+    for(int i = 1; i <= n; i++)
+    {
+        printf("%i choose %2i : %i\n", n, i, choose(n, i));
     }
 
     return 0;
