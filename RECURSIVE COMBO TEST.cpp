5801d5c30d387496bde9b5d43c1a5bf7cd840e1e
Aaron <avanclea@cs.uoregon.edu>
2020-01-26 23:13:02 -0800

    starter files, working on a basic factorial method to start an n choose m method

---
+++ RECURSIVE COMBO TEST.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+//factorial method
+//please please don't pass anything negative
+int fact(int n)
+{
+    int result = 1; //0!
+
+    for(int i = n; i > 0; i--)
+    {
+        result *= i;
+    }
+
+
+
+}
