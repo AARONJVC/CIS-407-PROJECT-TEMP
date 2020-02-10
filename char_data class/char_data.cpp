38094eb7b028af37ec9bf813430447c05430f3fe
Aaron <avanclea@cs.uoregon.edu>
2020-02-10 09:33:02 -0800

    added char_data class to store single characters and an index to simplify the process of retrieving messages. I think this entire project could be implemented without this, but it might be messier. Good to have.

---
+++ char_data class/char_data.cpp
@@ -0,0 +1,17 @@
+#include "char_data.h"
+
+char_data::char_data()
+{
+    //ctor
+}
+
+char getC();
+        int getI();
+
+        void setC(char);
+        void setI(int)
+
+char_data::~char_data()
+{
+    //dtor
+}
---
+++ char_data class/char_data.h
@@ -0,0 +1,25 @@
+#ifndef CHAR_DATA_H
+#define CHAR_DATA_H
+
+
+class char_data
+{
+    public:
+        char_data();
+
+        char getC();
+        int getI();
+
+        void setC(char);
+        void setI(int)
+
+        ~char_data();
+
+    private:
+
+        char cdata;
+
+        int index;
+};
+
+#endif // CHAR_DATA_H
